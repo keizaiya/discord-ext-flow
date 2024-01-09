785db1e15878f9335225ef5c8728427fa758c9fb
hawk-tomy <67221751+hawk-tomy@users.noreply.github.com>
2024-01-09 11:20:02 +0900

    update: allow Messageable for invoke and refactor message send helper

--- discord/ext/flow/controller.py
+++ discord/ext/flow/controller.py
@@ -4,12 +4,14 @@ from typing import TYPE_CHECKING
 
 from discord.utils import maybe_coroutine
 
+from .util import sender
 from .view import _View
 
 if TYPE_CHECKING:
     from typing import Self
 
-    from discord import Client, Interaction, InteractionMessage, WebhookMessage
+    from discord import Client, Interaction, Message as Message_
+    from discord.abc import Messageable
 
     from .model import Message, ModelBase
 
@@ -38,33 +40,33 @@ class Controller:
         """
         return self.__class__(self.model)
 
-    async def invoke(self, interaction: Interaction[Client]) -> None:
+    async def invoke(self, messageable: Messageable | Interaction[Client]) -> None:
         """Invoke flow.
 
         Args:
-            interaction (Interaction): Interaction to invoke. This interaction will be used to send first message.
+            messageable (Messageable | Interaction): Messageable or interaction to send first message.
         """
         model_or_msg: ModelBase = self.model
         while True:
-            if (ret := await self._send(model_or_msg, interaction)) is None:
+            if (ret := await self._send(model_or_msg, messageable)) is None:
                 break
-            model_or_msg, interaction = ret
+            model_or_msg, messageable = ret
 
     async def _send(
         self,
         model: ModelBase,
-        interaction: Interaction[Client],
+        messageable: Messageable | Interaction[Client],
     ) -> tuple[ModelBase, Interaction[Client]] | None:
         await maybe_coroutine(model.before_invoke)
         msg = await maybe_coroutine(model.message)
 
         if msg.items is None:
-            await self._interaction_send(interaction, msg, None)
+            await self._interaction_send(messageable, msg, None)
             await maybe_coroutine(model.after_invoke)
             return None
 
         view = _View(await maybe_coroutine(model.view_config), msg.items)
-        message = await self._interaction_send(interaction, msg, view)
+        message = await self._interaction_send(messageable, msg, view)
         await view.wait()
         if msg.disable_items:
             for child in view.children:
@@ -74,20 +76,10 @@ class Controller:
         return view.result
 
     async def _interaction_send(
-        self, interaction: Interaction[Client], message: Message, view: _View | None
-    ) -> WebhookMessage | InteractionMessage:
+        self, messageable: Messageable | Interaction[Client], message: Message, view: _View | None
+    ) -> Message_:
         kwargs = message._to_dict()
         if view is not None:
             kwargs['view'] = view
 
-        msg: WebhookMessage | InteractionMessage
-        if interaction.response.is_done():
-            msg = await interaction.followup.send(**kwargs, wait=True)
-        else:
-            await interaction.response.send_message(**kwargs)
-            msg = await interaction.original_response()
-
-        if message.delete_after is not None:
-            await msg.delete(delay=message.delete_after)
-
-        return msg
+        return await sender(messageable, **kwargs)
--- discord/ext/flow/util.py
+++ discord/ext/flow/util.py
@@ -1,9 +1,22 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, TypedDict
+
+from discord import Interaction
 
 if TYPE_CHECKING:
-    from typing import TypeVar
+    from collections.abc import Sequence
+    from typing import TypeGuard, TypeVar, Unpack
+
+    from discord import (
+        AllowedMentions,
+        Client,
+        Embed,
+        File,
+        Message,
+    )
+    from discord.abc import Messageable
+    from discord.ui import View
 
     T = TypeVar('T')
     U = TypeVar('U')
@@ -14,3 +27,62 @@ def unwrap_or(value: T | None, default: U) -> T | U:
     if value is None:
         return default
     return value
+
+
+class _SendKwType(TypedDict, total=False):
+    content: str
+    tts: bool
+    embeds: Sequence[Embed]
+    files: Sequence[File]
+    allowed_mentions: AllowedMentions
+    view: View
+    ephemeral: bool
+    suppress_embeds: bool
+    silent: bool
+
+
+class _MessageableSendKwType(TypedDict, total=False):
+    content: str
+    tts: bool
+    embeds: Sequence[Embed]
+    files: Sequence[File]
+    allowed_mentions: AllowedMentions
+    view: View
+    suppress_embeds: bool
+    silent: bool
+    delete_after: float
+
+
+def _is_messageable_kw(kwargs: _SendKwType) -> TypeGuard[_MessageableSendKwType]:
+    kwargs.pop('ephemeral', None)
+    return True
+
+
+async def sender(
+    messageable: Messageable | Interaction[Client],
+    *,
+    delete_after: float | None = None,
+    **kwargs: Unpack[_SendKwType],
+) -> Message:
+    """Helper function to send message. use messageable or interaction."""
+    if isinstance(messageable, Interaction):
+        msg: Message
+        if messageable.response.is_done():
+            msg = await messageable.followup.send(**kwargs, wait=True)
+        else:
+            await messageable.response.send_message(**kwargs)
+            msg = await messageable.original_response()
+
+        if delete_after is not None:
+            await msg.delete(delay=delete_after)
+
+        return msg
+    # if Messageable
+    kw: _SendKwType = {**kwargs}
+    if _is_messageable_kw(kw):
+        kwargs_: _MessageableSendKwType = kw
+        if delete_after is not None:
+            kwargs_['delete_after'] = delete_after
+        return await messageable.send(**kwargs_)
+
+    raise AssertionError('unreachable')
