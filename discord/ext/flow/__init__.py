6949a219d299c9394fb4733c544e3e8617b54615
hawk-tomy <67221751+hawk-tomy@users.noreply.github.com>
2024-03-01 18:35:37 +0900

    refactor __init__.py

--- discord/ext/flow/__init__.py
+++ discord/ext/flow/__init__.py
@@ -2,32 +2,27 @@ from __future__ import annotations
 
 __version__ = '0.1.4'
 
-from typing import TYPE_CHECKING
-
 from .controller import *
 from .modal import *
 from .model import *
 from .pages import *
 from .result import *
 
-if TYPE_CHECKING:
-    __all__ = (
-        'Message',
-        'Button',
-        'Link',
-        'Select',
-        'UserSelect',
-        'RoleSelect',
-        'MentionableSelect',
-        'ChannelSelect',
-        'ModelBase',
-        'Controller',
-        'TextInput',
-        'ModalConfig',
-        'send_modal',
-        'Paginator',
-        'paginator',
-        'Result',
-    )
-else:
-    __all__ = model.__all__ + controller.__all__ + modal.__all__ + pages.__all__ + result.__all__
+__all__ = (
+    'Message',
+    'Button',
+    'Link',
+    'Select',
+    'UserSelect',
+    'RoleSelect',
+    'MentionableSelect',
+    'ChannelSelect',
+    'ModelBase',
+    'Controller',
+    'TextInput',
+    'ModalConfig',
+    'send_modal',
+    'Paginator',
+    'paginator',
+    'Result',
+)
